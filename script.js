36bfc2ab869558adcdb706326b9594d6f359cda6
abdul <abdulmcajh@gmail.com>
2023-03-28 20:53:54 +0530

    second commit

--- script.js
+++ script.js
@@ -9,4 +9,81 @@
 // marks2 = 10
 // console.log(marks1, marks2)
 
+// x = 5;
+// y = 6;
+// z = x + y;
+// console.log(z)
 
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+// console.log(z)
+
+// const x = 5;
+// const y = 6;
+// const z = x + y;
+// console.log(z)
+
+// let price1 = 5;
+// const price2 = 6;
+// const total = price1 + price2;
+// total = price1 + price2 + 20;
+// price1=10
+
+// console.log(total)
+
+
+
+// function print()
+// {   
+    
+//     {
+//         var a=10; 
+//     }
+//     console.log(a)   
+// }
+
+// print()
+
+
+// const a=10
+// a=20
+// console.log(a)
+
+// var x; // x is the name of the variable
+// console.log(x); // undefined
+
+// var message="Hello";
+// var message="World"
+
+// message=10
+// message=20 
+// console.log(message)
+
+// let new  = 5
+// console.log(new)
+
+
+
+// function abc(){  
+//     let x = 10; // JavaScript local variable  
+// }
+
+// console.log(x)
+
+// let x, y, z;    // Statement 1
+// x = 5, y = 6;    // Statement 2 , // Statement 3
+// z = x + y;      // Statement 4
+// var a = 10;     // Statement 5
+// var b = 20;     // Statement 6
+
+// let a, b, c;  // Declare 3 variables
+// a = 5;        // Assign the value 5 to a
+// b = 6;        // Assign the value 6 to b
+// c = a + b;    // Assign the sum of a and b to c
+
+// a = 5; b = 6; c = a + b;
+
+
+let person =     "Mohan";
+console.log(person)
