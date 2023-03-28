46915778e810840c06f02fb3a9ed651f6fbf5884
abdul <abdulmcajh@gmail.com>
2023-03-28 19:50:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.write("Hello World ! Js in Head")
+
+// console.log("Hello world js in console")
+// console.info("Hello world js in console")
+// console.error("Hello world js in console")
+
+
+// marks1 = 90;
+// marks2 = 10
+// console.log(marks1, marks2)
+
+
