84c818928ce6b1e37ac904443eecd36b8e50b03a
abdul <abdulmcajh@gmail.com>
2023-03-29 21:04:23 +0530

    day 2 class code

---
+++ Day-2/script.js
@@ -0,0 +1,62 @@
+// Local Scope global scope
+
+// var x=10;// global variable
+// function sum() 
+// {
+//     var y=20; // local variable
+//     {
+//         let z=30 // local variable
+//     }
+//     console.log(z)
+// }
+// sum()
+
+//console.log(y)
+
+//var let const 
+// var - global & function
+// let - global & block
+// const - global & block
+
+// let a, b
+// ;
+// ;
+// c = 10
+
+
+// console.log(a,b,  "c = " , c)
+
+
+// ;
+
+
+// let a   =      10
+
+
+
+// console.log(a)
+
+// var 
+// x
+//  = 
+// 5
+// ;
+// var y = 10;
+// document.getElementById("demo").innerHTML = x 
+// +
+//  y;
+
+
+// var x = 5;
+// var y = 10;
+// var z = x + y;
+// alert(z);
+
+
+a=30
+b=20
+res= a>b ? "a is smaller" : "b is greter"
+
+console.log(res)
+
+
