2acabe5e5fe5dfc03310264bca821b33e2cc5e59
abdul <abdulmcajh@gmail.com>
2023-03-31 13:09:08 +0530

    Day 3 class code

---
+++ Day-3/script.js
@@ -0,0 +1,36 @@
+// let a=6
+// let b = a++
+
+// console.log(b, a)
+
+// let x= 6 << 2; 
+// // 0110 - 
+// console.log(x)
+
+
+// let a = 333333333333333333333
+// console.log(a)
+
+// let text = "ram"
+// let name = 'Ram'
+// let text2 = `Your namesdfsd465465654465d43543$#%$#%$#%43554 is ${name}`
+
+// console.log(text2)
+
+// const number1 = 3/0;
+// console.log(number1); // Infinity
+
+// const number2 = -3/0;
+// console.log(number2); // -Infinity
+
+// strings can't be divided by numbers
+// const number3 = "abc"/3; 
+// console.log(number3);  // NaN
+
+// let num=444;
+
+// console.log(typeof(num));
+
+let txt=`"abc"`
+
+console.log(txt)
