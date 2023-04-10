2e60b430bdaa0573c7d7292adea6b74619c9e1f1
abdul <abdulmcajh@gmail.com>
2023-04-11 00:00:58 +0530

    updated code till day10

---
+++ Day-9/script.js
@@ -0,0 +1,34 @@
+ let message ="Welcome to Primathon"
+
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+
+
+// console.log("2x1=2")
+// console.log("2x2=4")
+// console.log("2x3=6")
+// console.log("2x4=8")
+// console.log("2x5=10")
+// let i
+// for(;i<5;i++){
+//     console.log(message)
+
+// }
+    
+// console.log("i = " + i)
+
+
+
+// let n =5
+// for(let i=10, j=1, k=0; j<n; j++, i--){
+//     console.log(i)
+// }
+
+for (let j = 1;j<10 ; j += 2) {
+    console.log(j);
+    
+  }
+  
